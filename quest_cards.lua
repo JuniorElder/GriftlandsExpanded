b7ffe1ef6d8a3c6b686e76246306fdbb8f27b6f3
JuniorElder <64610396+JuniorElder@users.noreply.github.com>
2020-09-28 19:04:17 +0200

    tre
    gtd

---
+++ quest_cards.lua
@@ -0,0 +1,204 @@
+local negotiation_defs = require "negotiation/negotiation_defs"
+local CARD_FLAGS = negotiation_defs.CARD_FLAGS
+local EVENT = negotiation_defs.EVENT
+
+local CARDS = {
+    assassin_fight_call_for_help = 
+    {
+        name = "Call For Help",
+        desc = "Attempt to call for help and ask someone to deal with the assassin.",
+        cost = 1,
+        -- min_persuasion = 1,
+        -- max_persuasion = 3,
+        flags = CARD_FLAGS.DIPLOMACY | CARD_FLAGS.EXPEND,
+        rarity = CARD_RARITY.UNIQUE,
+        init_help_count = 1,
+
+        OnPostResolve = function( self, minigame, targets )
+            self.negotiator:AddModifier("CONNECTED_LINE", self.init_help_count)
+        end,
+        deck_handlers = ALL_DECKS,
+        event_handlers =
+        {
+            [ EVENT.CARD_MOVED ] = function( self, card, source_deck, source_idx, target_deck, target_idx )
+                if card == self and target_deck and target_deck:GetDeckType() == DECK_TYPE.TRASH 
+                    and self.negotiator:GetModifierStacks( "CONNECTED_LINE" ) <= 0 then
+                    self.show_dealt = true
+                    self:TransferCard(self.engine.hand_deck)
+                end
+            end,
+        },
+    },
+    assassin_fight_describe_information = 
+    {
+        name = "Describe Situation",
+        desc = "Discribe your current situation to the dispacher.\nIncrease the stacks of <b>Connected Line</> by 1.",
+        cost = 1,
+        -- min_persuasion = 1,
+        -- max_persuasion = 3,
+        flags = CARD_FLAGS.DIPLOMACY | CARD_FLAGS.STICKY,
+        rarity = CARD_RARITY.UNIQUE,
+        deck_handlers = ALL_DECKS,
+
+        OnPostResolve = function( self, minigame, targets )
+            if self.negotiator:GetModifierStacks( "CONNECTED_LINE" ) > 0 then
+                self.negotiator:AddModifier("CONNECTED_LINE", 1)
+            else
+                minigame:ExpendCard(self)
+            end
+        end,
+        event_handlers =
+        {
+            [ EVENT.CARD_MOVED ] = function( self, card, source_deck, source_idx, target_deck, target_idx )
+                if card == self and target_deck and target_deck:GetDeckType() ~= DECK_TYPE.IN_HAND then
+                    self.show_dealt = true
+                    if self.negotiator:GetModifierStacks( "CONNECTED_LINE" ) > 0 then
+                        local has_card = false
+                        for k,v in pairs(self.engine:GetHandDeck().cards) do
+                            if v.id == self.id and v ~= self then
+                                has_card = true
+                            end
+                        end
+                        if not has_card then
+                            self:TransferCard(self.engine.hand_deck)
+                        else
+                            self:TransferCard(self.engine.trash_deck)
+                        end
+                    else
+                        self:TransferCard(self.engine.trash_deck)
+                    end
+                end
+            end,
+        },
+    },
+    address_question = 
+    {
+        name = "Address Question",
+        desc = "Target a question argument. Resolve the effect based on the question being addressed. "..
+            "Remove the argument afterwards.\nIf this card leaves the hand, {EXPEND} it.",
+        loc_strings = {
+            NOT_A_QUESTION = "Target is not a question",
+        },
+        cost = 1,
+        flags = CARD_FLAGS.DIPLOMACY,
+        rarity = CARD_RARITY.UNIQUE,
+        target_enemy = TARGET_FLAG.ARGUMENT | TARGET_FLAG.BOUNTY,
+        deck_handlers = ALL_DECKS,
+        event_handlers =
+        {
+            [ EVENT.CARD_MOVED ] = function( self, card, source_deck, source_idx, target_deck, target_idx )
+                if card == self and target_deck and target_deck:GetDeckType() ~= DECK_TYPE.IN_HAND then
+                    self:TransferCard(self.engine.trash_deck)
+                end
+            end,
+        },
+        CanTarget = function(self, target)
+            if is_instance( target, Negotiation.Modifier ) and target.AddressQuestion then
+                return true
+            end
+            return false, self.def:GetLocalizedString("NOT_A_QUESTION")
+        end,
+        OnPostResolve = function( self, minigame, targets )
+            for i, target in ipairs(targets) do
+                if is_instance( target, Negotiation.Modifier ) and target.AddressQuestion then
+                    target:AddressQuestion()
+                    target:GetNegotiator():RemoveModifier( target )
+                end
+            end
+        end,
+    },
+    question_answer = 
+    {
+        name = "Question answer",
+        name_fn = function(self, fmt_str)
+            -- print("wololo")
+            -- print(self.issue_data)
+            -- print(self.stance)
+            if self.issue_data and self.stance then
+                -- print("narvini")
+                return self.issue_data.stances[self.stance]:GetLocalizedName()
+            end
+            return loc.format(fmt_str)
+        end,
+        desc = "This is a place that describes the answer to a question.",
+        desc_fn = function(self, fmt_str)
+            if self.issue_data and self.stance then
+                return self.issue_data.stances[self.stance]:GetLocalizedDesc()
+            end
+            return loc.format(fmt_str)
+        end,
+        flavour = "Here's what I think...",
+        loc_strings = {
+            ISSUE_DESC = "About <b>{1}</>:\n{2}",
+        },
+        flavour_fn = function(self, fmt_str)
+            if self.issue_data then
+                return loc.format(self.def:GetLocalizedString("ISSUE_DESC"), self.issue_data.name, self.issue_data.desc)
+            end
+            return loc.format(fmt_str)
+        end,
+        -- hide_in_cardex = true,
+        manual_desc = true,
+
+        cost = 0,
+        flags = CARD_FLAGS.UNPLAYABLE | CARD_FLAGS.DIPLOMACY,
+        rarity = CARD_RARITY.UNIQUE,
+
+        UpdateIssue = function(self, issue_data, stance)
+            self.issue_data = issue_data
+            self.stance = stance
+            -- self.title = self.issue_data.stances[self.stance].name
+            -- if self.engine then
+                self.engine:BroadcastEvent( EVENT.CARD_CHANGED, self, self:Clone() )
+            -- end
+        end,
+    },
+    
+    contemporary_question_card =
+    {
+
+        flags = CARD_FLAGS.SPECIAL | CARD_FLAGS.OPPONENT,
+        cost = 0,
+        stacks = 1,
+        rarity = CARD_RARITY.UNIQUE,
+
+        series = CARD_SERIES.NPC,
+
+        CanPlayCard = function( self, card, engine, target )
+            if card == self then
+                if not table.arraycontains(self.negotiator.behaviour.available_issues, self.issue_data) then
+                    self.issue_data = nil
+                end
+                if self.issue_data then
+                    return true
+                else
+                    self:TrySelectIssue()
+                    return self.issue_data ~= nil
+                end
+            end
+            return true
+        end,
+        TrySelectIssue = function(self)
+            if self.negotiator and self.negotiator.behaviour.available_issues then
+                self.issue_data = table.arraypick(self.negotiator.behaviour.available_issues)
+            end
+        end,
+
+        OnPostResolve = function( self, engine, targets )
+            local modifier = self.negotiator:CreateModifier("CONTEMPORARY_QUESTION")
+            if modifier then
+                modifier:SetIssue(self.issue_data)
+            end
+            if self.issue_data and self.negotiator.behaviour.available_issues then
+                table.arrayremove(self.negotiator.behaviour.available_issues, self.issue_data)
+                self.issue_data = nil
+            end
+        end,
+    },
+}
+for id, def in pairs( CARDS ) do
+    if not def.series then
+        def.series = CARD_SERIES.GENERAL
+    end
+    Content.AddNegotiationCard( id, def )
+end
